d76978e7e114a276d47c108ac682402f26321b5c
Hibiniy <mifels.fem@gmail.com>
2021-12-23 08:31:39 +0700

    now can get,post,put,delete

--- FunitureManager/Controllers/OrderController.cs
+++ FunitureManager/Controllers/OrderController.cs
@@ -1,29 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 using FunitureManager.Models;
-
 
 namespace FunitureManager.Controllers
 {
     public class OrderController : ApiController
     {
-        public IEnumerable<Order> Get()
+        private FunitureStoreDBContext db = new FunitureStoreDBContext();
+
+        // GET: api/Order
+        public IQueryable<Order> GetOrders()
         {
-            using (FunitureStoreDBContext dbContext = new FunitureStoreDBContext())
-            {
-                return dbContext.Orders.ToList();
-            }
+            return db.Orders;
         }
-        public Order Get(Guid id)
+
+        // GET: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetOrder(Guid id)
         {
-            using (FunitureStoreDBContext dbContext = new FunitureStoreDBContext())
+            Order order = db.Orders.Find(id);
+            if (order == null)
             {
-                return dbContext.Orders.FirstOrDefault(e => e.Id == id);
+                return NotFound();
             }
+
+            return Ok(order);
+        }
+
+        // PUT: api/Order/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrder(Guid id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Order
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult PostOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Orders.Add(order);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderExists(order.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult DeleteOrder(Guid id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(Guid id)
+        {
+            return db.Orders.Count(e => e.Id == id) > 0;
         }
     }
 }
