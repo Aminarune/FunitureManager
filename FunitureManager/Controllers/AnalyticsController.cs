24d9f14d7276e9dbc8b87b526ed8f19a67c27898
Hibiniy <mifels.fem@gmail.com>
2022-01-13 19:20:37 +0700

    add compare two years

--- FunitureManager/Controllers/AnalyticsController.cs
+++ FunitureManager/Controllers/AnalyticsController.cs
@@ -16,8 +16,9 @@ namespace FunitureManager.Controllers
         private FunitureStoreDBContext db = new FunitureStoreDBContext();
         // GET: Analytics
 
-        public ActionResult Indexanalytics(string year)
+        public ViewResult Indexanalytics(string year,string year2)
         {
+            //year1
             List<int> ye = new List<int>();
             int year1 = DateTime.Now.Year;
             ViewModel mymodel = new ViewModel();
@@ -31,6 +32,19 @@ namespace FunitureManager.Controllers
             }
             mymodel.Years = ye.ToList();
             int years = Int32.Parse(year);
+            //year2
+            List<int> ye2 = new List<int>();
+            if (year2 == null)
+            {
+                year2 = year1.ToString();
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                ye2.Add(year1 - i);
+            }
+            mymodel.Years = ye2.ToList();
+            int years2 = Int32.Parse(year2);
+            //
             List<int> listint = new List<int>();
             List<String> liststring = new List<String>();
             //product sold
@@ -58,6 +72,15 @@ namespace FunitureManager.Controllers
                 listint1.Add(orders);
             }
             ViewBag.ORDER = listint1;
+            //order2
+            List<int> listint12 = new List<int>();
+            for (int i = 0; i < 12; i++)
+            {
+                var orders = db.Orders.Where(x => x.Date.Year == years2).Where(x => x.Date.Month == i + 1).Count();
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", i + 1, orders);
+                listint12.Add(orders);
+            }
+            ViewBag.ORDER2 = listint12;
             //profit
             List<decimal?> listdec = new List<decimal?>();
             for (int i = 0; i < 12; i++)
@@ -74,7 +97,43 @@ namespace FunitureManager.Controllers
                 }
             }
             ViewBag.PROFIT = listdec;
-            
+            //profit2
+            List<decimal?> listdec2 = new List<decimal?>();
+            for (int i = 0; i < 12; i++)
+            {
+                decimal orders;
+                try
+                {
+                    orders = db.Orders.Where(x => x.Date.Year == years2).Where(x => x.Date.Month == i + 1).Sum(x => x.Price);
+                    listdec2.Add(orders);
+                }
+                catch (InvalidOperationException e)
+                {
+                    orders = 0;
+                    listdec2.Add(orders);
+                }
+            }
+            ViewBag.PROFIT2 = listdec2;
+            //favorite
+            List<int> listint2 = new List<int>();
+            List<String> liststring2 = new List<String>();
+            var fav = db.Favorites.GroupBy(x => x.Id_Product)
+                        .Select(x => new
+                        {
+                            id = x.Key,
+                            q = x.Count()
+                        }).OrderByDescending(z => z.q).ToList();
+            foreach (var row in fav)
+            {
+                Product p = db.Products.Find(row.id);
+                listint2.Add(row.q);
+                liststring2.Add(p.Name);
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", p.Name,row.q);
+            }
+            ViewBag.INT2 = listint2;
+            ViewBag.STRING2 = liststring2;
+            //compare
+
             return View(mymodel);
         }
     }
